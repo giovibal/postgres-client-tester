17e8443dab90c417160287f12b6749dff37aae5c
Giovanni Baleani <giovanni.baleani@gruppofilippetti.it>
2022-04-25 16:21:22 +0200

    cleaup code and refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::{env, thread};
 use std::time::Duration;
 use log::{debug, error, info};
-use postgres::{Client, Config, NoTls};
-use postgres::types::{Oid, Type};
+use postgres::{Config, NoTls};
 use clap::Parser;
+use r2d2::Pool;
 use r2d2_postgres::PostgresConnectionManager;
 
 #[derive(Parser)]
@@ -19,7 +19,51 @@ struct MyArgs {
 
 }
 
-fn main() -> Result<(), postgres::Error> {
+fn init_schema(pool: Pool<PostgresConnectionManager<NoTls>>) -> Result<(), postgres::Error> {
+    // get a client
+    let mut client = pool.get().unwrap();
+
+    // prepare DB: create a test table
+    client.batch_execute("
+        CREATE TABLE IF NOT EXISTS test_table  (
+            id              SERIAL PRIMARY KEY,
+            val             VARCHAR NOT NULL
+            )
+    ")
+}
+
+fn execute_insert_query(pool: Pool<PostgresConnectionManager<NoTls>>, counter: u128) {
+    // get a client
+    let mut client = pool.get().unwrap();
+
+    let query = "INSERT INTO test_table (val) VALUES ($1)";
+    let v = format!("rec n. {}", counter);
+    let result = client.execute(query, &[&v]);
+    if result.is_err() {
+        let err = result.err().unwrap();
+        error!("{}", err.to_string());
+    } else {
+        let cnt = result.unwrap();
+        info!("writed: {:?}", cnt);
+    }
+}
+
+fn execute_select_query(pool: Pool<PostgresConnectionManager<NoTls>>) {
+    // get a client
+    let mut client = pool.get().unwrap();
+
+    let result = client.query_one("SELECT count(*) FROM test_table", &[]);
+    if result.is_err() {
+        let err = result.err().unwrap();
+        error!("{}", err.to_string());
+    } else {
+        let cnt = result.unwrap();
+        let cnt: i64 = cnt.get(0);
+        info!("result: {:?}", cnt);
+    }
+}
+
+fn main() {
     env_logger::init();
     info!("log level: info");
     debug!("log level: debug");
@@ -36,75 +80,25 @@ fn main() -> Result<(), postgres::Error> {
     // let mut client = Client::connect(dbconn.as_str(), NoTls)?;
 
     // let mut config: Config = Config::from_str(dbconn.as_str()).expect("Connection url as: postgresql://postgres:Passw0rd@localhost:5432/tests");
-    let mut config: Config = dbconn.as_str().parse::<Config>().expect("Connection url as: postgresql://postgres:Passw0rd@localhost:5432/tests");
+    let config: Config = dbconn.as_str().parse::<Config>().expect("Connection url as: postgresql://postgres:Passw0rd@localhost:5432/tests");
     let manager = PostgresConnectionManager::new(config, NoTls);
     let pool = r2d2::Pool::new(manager).unwrap();
 
     // get a client
-    let pool = pool.clone();
-    let mut client = pool.get().unwrap();
-
-    // prepare DB: create a test table
-    client.batch_execute("
-        CREATE TABLE IF NOT EXISTS test_table  (
-            id              SERIAL PRIMARY KEY,
-            val             VARCHAR NOT NULL
-            )
-    ")?;
+    init_schema(pool.clone()).expect("Initialize schema");
 
     let mut counter : u128 = 0;
     loop {
-
-        // get a new client
-        let pool = pool.clone();
-        let mut client = pool.get().expect("a client from pool");
-
         match myargs.querytype {
             1 => {
-                let query = "INSERT INTO test_table (val) VALUES ($1)";
-                let v = format!("rec n. {}", counter);
-                let result = client.execute(query, &[&v]);
-                if result.is_err() {
-                    let err = result.err().unwrap();
-                    error!("{}", err.to_string());
-                    //return Result::Err(err);
-                } else {
-                    let cnt = result.unwrap();
-                    info!("writed: {:?}", cnt);
-                }
+                execute_insert_query(pool.clone(), counter);
             },
             2 => {
-                let result = client.query_one("SELECT count(*) FROM test_table", &[]);
-                if result.is_err() {
-                    let err = result.err().unwrap();
-                    error!("{}", err.to_string());
-                    // return Result::Err(err);
-                } else {
-                    let cnt = result.unwrap();
-                    let cnt: i64 = cnt.get(0);
-                    info!("result: {:?}", cnt);
-                }
+                execute_select_query(pool.clone());
             },
             _ => {
-                let query = "INSERT INTO test_table (val) VALUES ($1)";
-                let v = format!("rec n. {}", counter);
-                let result = client.execute(query, &[&v]);
-                if result.is_err() {
-                    let err = result.err().unwrap();
-                    error!("{}", err.to_string());
-                    // return Result::Err(err);
-                }
-
-                let result = client.query_one("SELECT count(*) FROM test_table", &[]);
-                if result.is_err() {
-                    let err = result.err().unwrap();
-                    error!("{}", err.to_string());
-                    // return Result::Err(err);
-                } else {
-                    let cnt = result.unwrap();
-                    let cnt: i64 = cnt.get(0);
-                    info!("result: {:?}", cnt);
-                }
+                execute_insert_query(pool.clone(), counter);
+                execute_select_query(pool.clone());
             },
         }
 
@@ -112,5 +106,4 @@ fn main() -> Result<(), postgres::Error> {
         thread::sleep(Duration::from_millis(myargs.sleep));
     }
 
-    Ok(())
 }
